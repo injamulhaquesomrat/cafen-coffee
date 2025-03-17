fe264c96e15947a6762f344162ef0bc30d5c9604
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-03-17 11:51:56 +0600

    add Hero component to HomeTwo page; implement SEO enhancements with Helmet

---
+++ src/components/Home2/Hero.jsx
@@ -0,0 +1,98 @@
+import Container from "../shared/Container";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <section className="bg-cf-light-yellow pt-8 overflow-hidden">
+      <Container>
+        <div className="flex flex-col sm:flex-row gap-8 justify-between">
+          {/* start:: hero text container */}
+          <div className="w-full md:w-3/6 flex flex-col justify-center py-10 lg:py-0 gap-[30px] md:gap-[50px] md:basis-2/3 lg:basis-1/2">
+            <h1 className="text-[46px] md:text-5xl xl:text-[80px] leading-tight tracking-tighter font-nunito font-bold text-cf-coffee">
+              Discover the perfect brew at Cafen café
+            </h1>
+
+            {/*start::hero menu button container */}
+            <div>
+              <Link to="/menu">
+                <button className="cf-btn text-cf-coffee relative overflow-hidden group mb-1 z-10">
+                  <img
+                    className="absolute top-0 -left-8 group-hover:-left-2 transition-all duration-500"
+                    src="/button-vector-01.svg"
+                    alt="button hover"
+                  />
+                  View our menu
+                  <img
+                    className="absolute -bottom-2 -right-10 group-hover:-right-5 transition-all duration-500"
+                    src="/button-vector-02.svg"
+                    alt="button hover"
+                  />
+                </button>
+              </Link>
+              <div className="flex items-center gap-2">
+                <img
+                  src="https://cdn.prod.website-files.com/66b98cdd4dab60f19c8c1440/66c479e0032ed262ed70798a_star-one.svg"
+                  alt="star icon"
+                />
+                <span className="text-cf-coffee">
+                  4.95/5 from over 100 reviews
+                </span>
+              </div>
+            </div>
+            {/* end::hero menu button container*/}
+
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 lg:gap-x-8 xl:gap-y-0 text-cf-coffee ">
+              <div className="flex items-start gap-3">
+                <div className="bg-cf-yellow p-1.5 md:p-3.5 rounded-xl border border-cf-coffee w-fit">
+                  <img
+                    src="/icons/about/about-icon-01.svg"
+                    alt="coffee ICON"
+                  />
+                </div>
+                <div>
+                  <h6 className="text-xl lg:text-2xl font-semibold font-nunito tracking-tighter">
+                  Artisanal Coffee
+                  </h6>
+                  <p className="font-nunito tracking-tight lg:text-lg">
+                    Savor fresh, locally sourced menu items.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-start gap-3">
+                <div className="bg-cf-yellow p-1.5 md:p-3.5 rounded-xl border border-cf-coffee w-fit">
+                  <img
+                    src="/icons/about/about-icon-03.svg"
+                    alt="ABOUT ICON"
+                  />
+                </div>
+                <div>
+                  <h6 className="text-xl lg:text-2xl font-semibold font-nunito tracking-tighter">
+                    Free Wi-Fi
+                  </h6>
+                  <p className="font-nunito tracking-tight lg:text-lg">
+                    Stay connected with our complimentary Wi-Fi.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* end:: hero text container */}
+
+          {/* start::hero banner content */}
+          <div className="-mb-6 -mt-14 md:-mt-16 md:-mr-4 lg:mr-0 lg:-mt-32 lg:-mb-36 -rotate-6 md:basis-1/3 lg:basis-1/2">
+            <div>
+              <img
+              className="w-[600px] h-[600px] lg:h-[800px] lg:w-[800px] xl:w-full xl:h-full object-cover"
+                src="https://cdn.prod.website-files.com/66b98cdd4dab60f19c8c1440/66c97d18d320003d1a1fcc6f_hero-image-02.avif"
+                alt="hero banner image"
+              />
+            </div>
+          </div>
+          {/* end::hero banner content */}
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Hero;
--- src/pages/HomeTwo.jsx
+++ src/pages/HomeTwo.jsx
@@ -1,7 +1,15 @@
+import { Helmet } from "react-helmet";
+import Hero from "../components/Home2/Hero";
+
 const HomeTwo = () => {
   return (
     <>
-      <h1>Hey This is HomeTwo</h1>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home2 | Cafen - Ecommerce Website</title>
+        <link rel="canonical" href="/" />
+      </Helmet>
+      <Hero />
     </>
   );
 };
