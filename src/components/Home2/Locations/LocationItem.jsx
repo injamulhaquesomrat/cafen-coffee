b5a77db8f9702cd09b6032aab613b5d977506bce
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-03-26 21:11:41 +0600

    refactor LocationItem component; enhance layout and improve responsiveness

--- src/components/Home2/Locations/LocationItem.jsx
+++ src/components/Home2/Locations/LocationItem.jsx
@@ -5,33 +5,33 @@ const LocationItem = ({ item }) => {
   const { title, image, location, mapLink, contact } = item;
 
   return (
-    <div className="p-5 border border-cf-coffee bg-cf-foreground rounded-2xl space-y-4">
-      <div>
+    <div className="p-5 border border-cf-coffee bg-cf-foreground rounded-2xl space-y-4 h-full">
+      <div className="">
         <img
           className="aspect-square object-cover rounded-xl"
           src={image}
           alt={title}
         />
       </div>
-      <div>
-        <h2 className="text-2xl text-cf-coffee text-nunito tracking-tighter leading-tight pb-2">
-          {title}
-        </h2>
-        <p className="text-cf-coffee">
-          {location}
-        </p>
-        <div className="pt-[50px] mb-0 flex flex-col text-cf-coffee font-medium gap-4 justify-between">
+      <div className="aspect-[16/12] lg:aspect-square flex flex-col justify-between">
+        <div className="mb-8 h-fit">
+          <h2 className="text-2xl text-cf-coffee text-nunito tracking-tighter leading-tight pb-2">
+            {title}
+          </h2>
+          <p className="text-cf-coffee">{location}</p>
+        </div>
+        <div className="flex flex-col xl:flex-row xl:items-center text-cf-coffee font-medium gap-4 justify-between">
           <a className="hover:underline" href={`tel:${contact}`}>
             {contact}
           </a>
           <Link href={mapLink}>
-          <button className="inline-flex px-6  bg-cf-yellow py-3 rounded-full items-center gap-2 group border border-cf-coffee w-fit">
-            <span>View on map</span>{" "}
-            <FaArrowRight
-              className="group-hover:translate-x-1 duration-100 ease-in-out"
-              size={12}
-            />
-          </button>
+            <button className="inline-flex px-6  bg-cf-yellow py-3 rounded-full items-center gap-2 group border border-cf-coffee w-fit">
+              <span>View on map</span>{" "}
+              <FaArrowRight
+                className="group-hover:translate-x-1 duration-100 ease-in-out"
+                size={12}
+              />
+            </button>
           </Link>
         </div>
       </div>
