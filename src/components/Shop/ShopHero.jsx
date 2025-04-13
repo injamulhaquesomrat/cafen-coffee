981c3fdfb3d69021cd9f7d5eead22ab7a22327a7
haquesomrat <ihsomrat.official@gmail.com>
2025-04-13 19:23:20 +0600

    add PageHero component; refactor ShopHero and Menu components to utilize PageHero for improved layout and structure

--- src/components/Shop/ShopHero.jsx
+++ src/components/Shop/ShopHero.jsx
@@ -1,3 +1,4 @@
+import PageHero from "../shared/PageHero";
 import ShopTabs from "./ShopTabs";
 
 const shopTabItems = [
@@ -21,19 +22,18 @@ const shopTabItems = [
 
 const ShopHero = ({ selectedCategory, handleCategoryChange }) => {
   return (
-    <div className="py-[80px] px-5 bg-cf-light-yellow border border-cf-coffee rounded-lg shadow-lg mt-5">
-      <h1 className="text-[50px] text-center md:text-7xl leading-tighter tracking-tighter font-nunito font-bold text-cf-coffee">
-        Shop
-      </h1>
-
-      <div className="flex flex-col sm:flex-row justify-center items-center gap-2 md:gap-6 mt-5 lg:mt-8 flex-wrap">
-        <ShopTabs
-          shopTabItems={shopTabItems}
-          selectedCategory={selectedCategory}
-          handleCategoryChange={handleCategoryChange}
-        />
-      </div>
-    </div>
+    <>
+      <PageHero
+        pageTitle={"Shop"}
+        subContent={
+          <ShopTabs
+            shopTabItems={shopTabItems}
+            selectedCategory={selectedCategory}
+            handleCategoryChange={handleCategoryChange}
+          />
+        }
+      />
+    </>
   );
 };
 
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,8 +1,32 @@
+import { Helmet } from "react-helmet";
+import Container from "../components/shared/Container";
+import PageHero from "../components/shared/PageHero";
+import GoToTop from "../components/shared/GoToTop";
+
 const Menu = () => {
   return (
-    <div>
-      <h1>Hey This is Menu</h1>
-    </div>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Menu | Cafen </title>
+        <link rel="canonical" href="/" />
+      </Helmet>
+      <div>
+        <Container>
+          <PageHero
+            pageTitle={"Our Menu"}
+            subContent={
+              <p className="text-cf-coffee max-w-[620px] text-center mx-auto">
+                Welcome to Cafen! Here, we take pride in serving a curated
+                selection of drinks and delights that are sure to satisfy every
+                palate.
+              </p>
+            }
+          />
+        </Container>
+        <GoToTop />
+      </div>
+    </>
   );
 };
 
