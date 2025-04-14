31a9805de7871c7c2aa7b63c17f5a56fe1105994
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-04-14 18:43:03 +0600

    refactor Blog component; implement pagination logic and update item display for improved user experience

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,8 +5,17 @@ import GoToTop from "../components/shared/GoToTop";
 import ShopPagination from "../components/Shop/ShopPagination";
 import { blogItems } from "../utils/blogItems";
 import BlogCard from "../components/Blog/BlogCard";
+import { useState } from "react";
 
 const Blog = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = blogItems.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(blogItems.length / itemsPerPage);
   return (
     <>
       <Helmet>
@@ -21,17 +30,17 @@ const Blog = () => {
           {/* start:: products container */}
           <div className="py-[60px] sm:py-20 lg:py-[100px]">
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 ">
-              {blogItems.map((item) => (
+              {currentItems.map((item) => (
                 <BlogCard key={item?.id} item={item} />
               ))}
             </div>
 
             {/* start:: pagination */}
-            {/* <ShopPagination
+            <ShopPagination
               currentPage={currentPage}
               setCurrentPage={setCurrentPage}
               totalPages={totalPages}
-            /> */}
+            />
             {/* end:: pagination */}
           </div>
 
