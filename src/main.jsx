def3ed9f8383cc8f00e5b99d9844abba6d3636e0
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-04-20 14:19:18 +0600

    feat: add font-face definitions for BD Grotesk and Sober fonts; update index.html to preload Sober font

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "./fonts.css";
 import { RouterProvider } from "react-router-dom";
 import Router from "./routes/Router.jsx";
 import "@splidejs/splide/dist/css/splide.min.css";
