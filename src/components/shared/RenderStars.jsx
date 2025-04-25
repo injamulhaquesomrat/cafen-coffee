91b5ba4520cac4d8d3f02a1dcd9abb5517e4c361
Injamul Haque Somrat <ihsomrat.official@gmail.com>
2025-04-25 18:46:50 +0600

    refactor: Replace GiRoundStar with img elements for star rendering in RenderStars component

--- src/components/shared/RenderStars.jsx
+++ src/components/shared/RenderStars.jsx
@@ -1,10 +1,14 @@
-import { GiRoundStar } from "react-icons/gi";
+
 
 const RenderStars = ({ count }) => {
   return (
     <div className="inline-flex gap-1 text-[#ffc700] items-center">
       {Array.from({ length: count }, (_, index) => (
-        <GiRoundStar key={index} className="text-2xl" />
+        <img
+          key={index}
+          src="/assets/icons/props/star-one.svg"
+          alt="star one"
+        />
       ))}
     </div>
   );
